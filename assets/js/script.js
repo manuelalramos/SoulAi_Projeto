4e194396472ff9bd0c920d07196caf32cdc647ca
manuelalramos <manuela.lima.r@outlook.com>
2026-05-14 23:33:06 -0300

    feat : Adicionando defer no link do JS. Criando a lógica dos cards em JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,28 @@ if (cardSlider) {
     //Capturando o botão direito
     const rightButton = cardSlider.querySelector("#button-right");
     //Capturando as bolinhas de status dos cards
-    const sliderDots = cardSlider.querySelector(".slider-dot")
+    const sliderDots = cardSlider.querySelector(".slider-dot");
+
+    //Card do momento
+    let currentCard = 0;
+
+    //Função que mostra apenas o card escolhido
+    function showCard(cardIndex) {
+        //Tirando a classe active 
+        sliderCards.forEach(function(card) {
+            card.classList.remove("active")
+        });
+        //Active apenas no que deve aparecer
+        sliderCards[cardIndex].classList.add("active");
+
+        //Tirando o active das bolinhas
+        sliderDots.forEach(function (dot) {
+            dot.classList.remove("active");
+        });
+
+        //Active da bolinha apenas no card atual
+        sliderDots[cardIndex].classList.add("active");
+    }
 
     
 }
