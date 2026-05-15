eafdb4cef7210b68fce5a56105084a25c1717775
manuelalramos <manuela.lima.r@outlook.com>
2026-05-14 23:26:09 -0300

    feat : Iniciando o JS. Capturando os elementos dos cards.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+//Capturando a div slider
+const cardSlider = document.querySelector(".cards-slider");
+//Verificando se existe o slider na página antes
+if (cardSlider) {
+    //Capturando todos os cards
+    const sliderCards = cardSlider.querySelector(".slider-card");
+    //Capturando o botão esquerda
+    const leftButton = cardSlider.querySelector("#button-left");
+    //Capturando o botão direito
+    const rightButton = cardSlider.querySelector("#button-right");
+    //Capturando as bolinhas de status dos cards
+    const sliderDots = cardSlider.querySelector(".slider-dot")
+
+    
+}
