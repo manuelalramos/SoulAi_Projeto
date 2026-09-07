a0fc7a1e5e6b40e673e5b9e49740d4233f1edf58
manuelalramos <manuela.lima.r@outlook.com>
2026-09-07 17:49:45 -0300

    refactor : adiciona types compartilhado

---
+++ src/types/project.ts
@@ -0,0 +1,17 @@
+export type NavItem = {
+ label: string;
+ to: string;
+};
+export type TeamMember = {
+ id: string;
+ name: string;
+ role: string;
+ image: string;
+ github?: string;
+ linkedin?: string;
+};
+export type SolutionFeature = {
+ slug: string;
+ title: string;
+ description: string;
+};
