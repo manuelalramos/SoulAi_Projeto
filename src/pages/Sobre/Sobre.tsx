e55d54f309bdf368fee1495f2913bb7c06b74eab
manuelalramos <manuela.lima.r@outlook.com>
2026-09-08 14:24:13 -0300

    fix : corrige erro ortografico

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -9,8 +9,7 @@ export default function Sobre() {
             <PageIntro
                 label="Sobre o projeto"
                 title="SoulUp + SoulAI"
-                description="Conheça a proposta do SoulAI, um avatar inteligente criado para tornar a experiência na
-SoulUp mais interativa, personalizada e conectada com ações sustentáveis."
+                description="Conheça a proposta do SoulAI, um avatar inteligente criado para tornar a experiência na SoulUp mais interativa, personalizada e conectada com ações sustentáveis."
             />
             <AboutObjectiveSection />
             <AboutHighlightsSection />
