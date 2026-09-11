add9ac5e446edf019be4191c704fd49cf44e2374
lyviaamorim569851 <lyviacorreadeamorim@gmail.com>
2026-09-10 22:51:12 -0300

    fix: corrige mensagem de missoes do SoulAI

--- src/data/chat.ts
+++ src/data/chat.ts
@@ -9,7 +9,7 @@ export const chatMessages: ChatMessage[] = [
     { author: "SoulAI", text: "Ei, que bom que voltou hoje! 🔥\nSua sequência continua ativa!" },
     { author: "SoulAI", text: "O que deseja fazer hoje?" },
     { author: "Usuário", text: "Ver minhas missões" },
-    { author: "SoulAI", text: "Você tem 2 missões disponíveis hoje \n• Assistir um conteúdo sustentável (+2 moedas)\n• 👇Completar uma ação consciente (+5 moedas)" },
+    { author: "SoulAI", text: "Você tem 2 missões disponíveis hoje 👇\n• Assistir um conteúdo sustentável (+2 moedas)\n• Completar uma ação consciente (+5 moedas)" },
     { author: "Usuário", text: "Quero fazer uma ação rápida" },
     { author: "SoulAI", text: "Perfeito 🙂\nMe responde uma coisa:\nHoje você fez algo pra economizar energia?" },
     { author: "Usuário", text: "Sim, desliguei as luzes antes de sair" },
